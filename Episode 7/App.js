07b016043fe637935ef3c8088bd481bfe62c2116
kartikey151999 <ankurpande159@gmail.com>
2025-01-01 12:32:41 +0530

    ep7

---
+++ Episode 7/App.js
@@ -0,0 +1,50 @@
+import React from "react"
+import ReactDOM from "react-dom/client"
+import Header from "./Src/components/Header" 
+import Body from "./Src/components/Body"
+import { createBrowserRouter,RouterProvider,Outlet } from "react-router-dom"
+import Error from "./Src/components/Error"
+import Aboutus from "./Src/components/Aboutus"
+import Contact from "./Src/components/Contact"
+import RestaurantMenu from "./Src/components/RestaurantMenu"
+
+const Applayout = () =>{
+ return(
+    <div className="app">
+    <Header/>
+    <Outlet/>
+    </div>
+ )
+
+}
+const route = createBrowserRouter([
+{
+  path:"/",
+  element:<Applayout/>,  
+  children:[
+{
+  path : "/",
+  element : <Body/>     
+  
+},
+{
+   path:"/About",
+   element : <Aboutus/>,  
+},
+{
+   path:"/Contact",
+   element: <Contact/>,
+},
+{
+ path:"/restaurant/:resId",
+ element:<RestaurantMenu/>  
+}
+],
+errorElement : <Error/>,
+},
+]);
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
+root.render(<RouterProvider router={route} />) // render functional component.
---
+++ Episode 7/Src/components/Shimmer.js
@@ -0,0 +1,122 @@
+const Shimmer = () =>{
+    return(
+    <div className="shimmer-cnt">
+     <div className="shimmer">
+        <img className="dummy-img"  />
+        <div className="dummy-data"></div>
+        <div className="dummy-data"></div>
+        <div className="dummy-data"></div>
+        <div className="dummy-data"></div>
+   </div>
+   <div className="shimmer">
+        <img className="dummy-img"  />
+        <div className="dummy-data"></div>
+        <div className="dummy-data"></div>
+        <div className="dummy-data"></div>
+        <div className="dummy-data"></div>
+   </div>
+   <div className="shimmer">
+        <img className="dummy-img"  />
+        <div className="dummy-data"></div>
+        <div className="dummy-data"></div>
+        <div className="dummy-data"></div>
+        <div className="dummy-data"></div>
+   </div>
+   <div className="shimmer">
+        <img className="dummy-img"  />
+        <div className="dummy-data"></div>
+        <div className="dummy-data"></div>
+        <div className="dummy-data"></div>
+        <div className="dummy-data"></div>
+   </div>
+   <div className="shimmer">
+        <img className="dummy-img"  />
+        <div className="dummy-data"></div>
+        <div className="dummy-data"></div>
+        <div className="dummy-data"></div>
+        <div className="dummy-data"></div>
+   </div>
+   <div className="shimmer">
+        <img className="dummy-img"  />
+        <div className="dummy-data"></div>
+        <div className="dummy-data"></div>
+        <div className="dummy-data"></div>
+        <div className="dummy-data"></div>
+   </div>
+   <div className="shimmer">
+        <img className="dummy-img"  />
+        <div className="dummy-data"></div>
+        <div className="dummy-data"></div>
+        <div className="dummy-data"></div>
+        <div className="dummy-data"></div>
+   </div>
+   <div className="shimmer">
+        <img className="dummy-img"  />
+        <div className="dummy-data"></div>
+        <div className="dummy-data"></div>
+        <div className="dummy-data"></div>
+        <div className="dummy-data"></div>
+   </div>
+   <div className="shimmer">
+        <img className="dummy-img"  />
+        <div className="dummy-data"></div>
+        <div className="dummy-data"></div>
+        <div className="dummy-data"></div>
+        <div className="dummy-data"></div>
+   </div>
+   <div className="shimmer">
+        <img className="dummy-img"  />
+        <div className="dummy-data"></div>
+        <div className="dummy-data"></div>
+        <div className="dummy-data"></div>
+        <div className="dummy-data"></div>
+   </div>
+   <div className="shimmer">
+        <img className="dummy-img"  />
+        <div className="dummy-data"></div>
+        <div className="dummy-data"></div>
+        <div className="dummy-data"></div>
+        <div className="dummy-data"></div>
+   </div>
+   <div className="shimmer">
+        <img className="dummy-img"  />
+        <div className="dummy-data"></div>
+        <div className="dummy-data"></div>
+        <div className="dummy-data"></div>
+        <div className="dummy-data"></div>
+   </div>
+   <div className="shimmer">
+        <img className="dummy-img"  />
+        <div className="dummy-data"></div>
+        <div className="dummy-data"></div>
+        <div className="dummy-data"></div>
+        <div className="dummy-data"></div>
+   </div>
+   <div className="shimmer">
+        <img className="dummy-img"  />
+        <div className="dummy-data"></div>
+        <div className="dummy-data"></div>
+        <div className="dummy-data"></div>
+        <div className="dummy-data"></div>
+   </div>
+   <div className="shimmer">
+        <img className="dummy-img"  />
+        <div className="dummy-data"></div>
+        <div className="dummy-data"></div>
+        <div className="dummy-data"></div>
+        <div className="dummy-data"></div>
+   </div>
+   <div className="shimmer">
+        <img className="dummy-img"  />
+        <div className="dummy-data"></div>
+        <div className="dummy-data"></div>
+        <div className="dummy-data"></div>
+        <div className="dummy-data"></div>
+   </div>
+    
+     
+    </div>
+    )
+}
+
+export default Shimmer;
